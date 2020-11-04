cfb54b3ebc7ee7b7895886578caf020fe69efbc1
daizack <zdai@rel8ed.to>
2020-11-04 14:57:48 +0000

    corpName

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zacktools", # Replace with your own username
-    version="0.0.15",
+    version="0.0.18",
     author="Zack Dai",
     author_email="zdai@brocku.ca",
     description="Zack's ommon tools",
